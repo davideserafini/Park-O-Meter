d1b5f57c43e0cb4e7be124d9247b5e43597e3b8e
Davide Serafini <davide@davideserafini.com>
2016-01-02 23:23:51 +0100

    Basic layout for first screen in progress

---
+++ app/src/main/java/com/davideserafini/parkometer/CalculateFeeActivity.java
@@ -0,0 +1,13 @@
+package com.davideserafini.parkometer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculateFeeActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_calculate_fee);
+	}
+}
